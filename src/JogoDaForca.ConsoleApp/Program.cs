5ffce2a1ba75ab560434e9a8b1c7e9a77c1879f7
chayannerech <rechchayanne@gmail.com>
2024-04-03 21:06:19 -0300

    Algumas refatoriações.

--- src/JogoDaForca.ConsoleApp/Program.cs
+++ src/JogoDaForca.ConsoleApp/Program.cs
@@ -9,26 +9,87 @@ namespace JogoDaForca.ConsoleApp
             {
                 string palavraAleatoria; char tentativa; int numErro; char[] caracteres, mostraTentativa;
                 IniciaVariaveis(out palavraAleatoria, out tentativa, out numErro, out caracteres, out mostraTentativa);
-
                 while (true)
                 {
                     Console.Clear();
                     int errou = 1;
-
                     MostrarForca(numErro, mostraTentativa, ref tentativa, palavraAleatoria);
 
                     if (numErro == 5) break;
 
+                    MostraAtual(mostraTentativa);
                     RecebeChute(caracteres, mostraTentativa, ref errou, ref numErro);
 
-                    if (Array.TrueForAll(mostraTentativa, valor => valor != '_'))
-                    {
-                        Venceu(palavraAleatoria, tentativa, numErro, mostraTentativa);
-                        break;
-                    }
+                    Venceu(palavraAleatoria, tentativa, numErro, mostraTentativa);
+                    if (Array.TrueForAll(mostraTentativa, valor => valor != '_')) break;
                 }
                 if (DeveContinuar()) break;
             }
+        }
+        static void IniciaVariaveis(out string palavraAleatoria, out char tentativa, out int numErro, out char[] caracteres, out char[] mostraTentativa)
+        {
+            Random random = new Random();
+
+            string[] palavra = { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJÁ", "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA", "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
+            palavraAleatoria = palavra[random.Next(palavra.Length)];
+            tentativa = ' ';
+            numErro = 0;
+            caracteres = palavraAleatoria.ToCharArray();
+            mostraTentativa = new char[caracteres.Length * 2];
+
+            for (int i = 0; i < mostraTentativa.Length; i++)
+            {
+                if (i % 2 == 0) mostraTentativa[i] = '_';
+                else mostraTentativa[i] = ' ';
+            }
+        }
+        static void MostrarForca(int numErro, char[] mostraTentativa, ref char tentativa, string palavraAleatoria)
+        {
+            switch (numErro)
+            {
+                case 0:
+                    Console.Write("  ___________\n  |/\n  |\n  |\n  |\n  |\n  |\n  |\n _|____\n\n   ");
+                    break;
+
+                case 1:
+                    Console.WriteLine("  ___________");
+                    Console.WriteLine("  |/       |");
+                    Console.Write("  |        O\n  |\n  |\n  |\n  |\n  |\n _|____\n\n   ");
+                    break;
+
+                case 2:
+                    Console.WriteLine("  ___________");
+                    Console.WriteLine("  |/       |");
+                    Console.WriteLine("  |        O");
+                    Console.Write("  |        X\n  |\n  |\n  |\n  |\n _|____\n\n   ");
+                    break;
+
+                case 3:
+                    Console.WriteLine("  ___________");
+                    Console.WriteLine("  |/       |");
+                    Console.WriteLine("  |        O");
+                    Console.Write("  |       /X\n  |\n  |\n  |\n  |\n _|____\n\n   ");
+                    break;
+
+                case 4:
+                    Console.WriteLine("  ___________");
+                    Console.WriteLine("  |/       |");
+                    Console.WriteLine("  |        O");
+                    Console.Write("  |       /X\\\n  |\n  |\n  |\n  |\n _|____\n\n   ");
+                    break;
+
+                case 5:
+                    Console.WriteLine("  ___________");
+                    Console.WriteLine("  |/       |");
+                    Console.WriteLine("  |        O");
+                    Console.WriteLine("  |       /X\\");
+                    Console.Write($"  |        X\n  |\n  |\n  |\n _|____\n\n\nFIM DE JOGO :(\nA palavra era: {palavraAleatoria}\n\n");
+                    break;
+            }
+        }
+        static void MostraAtual(char[] mostraTentativa)
+        {
+            for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
         }
         static void RecebeChute(char[] caracteres, char[] mostraTentativa, ref int errou, ref int numErro)
         {
@@ -48,104 +109,12 @@ namespace JogoDaForca.ConsoleApp
         }
         static void Venceu(string palavraAleatoria, char tentativa, int numErro, char[] mostraTentativa)
         {
-            Console.Clear();
-            MostrarForca(numErro, mostraTentativa, ref tentativa, palavraAleatoria);
-            Console.WriteLine("\n\nUAU!!! Você venceu!\n");
-        }
-        static void IniciaVariaveis(out string palavraAleatoria, out char tentativa, out int numErro, out char[] caracteres, out char[] mostraTentativa)
-        {
-            Random random = new Random();
-
-            string[] palavra = { "ABACATE", "ABACAXI", "ACEROLA", "AÇAÍ", "ARAÇA", "ABACATE", "BACABA", "BACURI", "BANANA", "CAJÁ", "CAJÚ", "CARAMBOLA", "CUPUAÇU", "GRAVIOLA", "GOIABA", "JABUTICABA", "JENIPAPO", "MAÇÃ", "MANGABA", "MANGA", "MARACUJÁ", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI", "TANGERINA", "UMBU", "UVA", "UVAIA" };
-            palavraAleatoria = palavra[random.Next(palavra.Length)];
-            tentativa = ' ';
-            numErro = 0;
-            caracteres = palavraAleatoria.ToCharArray();
-            mostraTentativa = new char[caracteres.Length * 2];
-            for (int i = 0; i < mostraTentativa.Length; i++)
+            if (Array.TrueForAll(mostraTentativa, valor => valor != '_'))
             {
-                if (i % 2 == 0) mostraTentativa[i] = '_';
-                else mostraTentativa[i] = ' ';
-            }
-        }
-        static void MostrarForca(int numErro, char[] mostraTentativa, ref char tentativa, string palavraAleatoria)
-        {
-            switch (numErro)
-            {
-                case 0:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write("\n   ");
-
-                    for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
-
-                    break;
-
-                case 1:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/       |");
-                    Console.WriteLine("  |        O");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write("\n   ");
-
-                    for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
-
-                    break;
-
-                case 2:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/       |");
-                    Console.WriteLine("  |        O");
-                    Console.WriteLine("  |        X");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write("\n   ");
-
-                    for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
-
-                    break;
-
-                case 3:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/       |");
-                    Console.WriteLine("  |        O");
-                    Console.WriteLine("  |       /X");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write("\n   ");
-
-                    for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
-
-                    break;
-
-                case 4:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/       |");
-                    Console.WriteLine("  |        O");
-                    Console.WriteLine("  |       /X\\");
-                    Console.WriteLine("  |");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write("\n   ");
-
-                    for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
-
-                    break;
-
-                case 5:
-                    Console.WriteLine("  ___________");
-                    Console.WriteLine("  |/       |");
-                    Console.WriteLine("  |        O");
-                    Console.WriteLine("  |       /X\\");
-                    Console.WriteLine("  |        X");
-                    Console.WriteLine("  |\n  |\n  |\n _|____");
-                    Console.Write($"\n\nFIM DE JOGO :(\nA palavra era: {palavraAleatoria}\n\n");
-                    break;
+                Console.Clear();
+                MostrarForca(numErro, mostraTentativa, ref tentativa, palavraAleatoria);
+                for (int i = 0; i < mostraTentativa.Length; i++) Console.Write(mostraTentativa[i]);
+                Console.WriteLine("\n\nUAU!!! Você venceu!\n");
             }
         }
         static bool DeveContinuar()
